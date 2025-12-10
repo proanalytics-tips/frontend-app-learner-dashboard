cb10fe34a246fc9542002f669e41f58728fcacf8
MariOdy <mariody.work@gmail.com>
2025-12-10 11:36:52 +0200

    fix: update page title and favicon URL in App component

--- src/messages.js
+++ src/messages.js
@@ -1,20 +1,21 @@
-import { defineMessages } from '@edx/frontend-platform/i18n';
+import { defineMessages } from "@edx/frontend-platform/i18n";
 
 const messages = defineMessages({
   loadingSR: {
-    id: 'learner-dash.loadingSR',
-    description: 'Page loading screen-reader text',
-    defaultMessage: 'Loading...',
+    id: "learner-dash.loadingSR",
+    description: "Page loading screen-reader text",
+    defaultMessage: "Loading...",
   },
   errorMessage: {
-    id: 'learner-dash.error-page-message',
-    defaultMessage: 'If you experience repeated failures, please email support at {supportEmail}',
-    description: 'Error page message',
+    id: "learner-dash.error-page-message",
+    defaultMessage:
+      "If you experience repeated failures, please email support at {supportEmail}",
+    description: "Error page message",
   },
   pageTitle: {
-    id: 'learner-dash.title',
-    description: 'Page title: Learner Home',
-    defaultMessage: 'Learner Home',
+    id: "learner-dash.title",
+    description: "Page title: Learner Home",
+    defaultMessage: "Dashboard",
   },
 });
 
